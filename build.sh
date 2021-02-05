c7fe4b934026e63c2fe0240c95921e56e98b2ec5
Torngate <davidsangrey@gmail.com>
2021-02-05 16:55:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cp m_autoban.c ../unrealircd-5.0.7/src/modules/third
-cd ../unrealircd-5.0.7/
+cp m_autoban.c ../unrealircd-5.0.8/src/modules/third
+cd ../unrealircd-5.0.8/
 make
 cp src/modules/third/m_autoban.so ../unrealircd/modules/third/
